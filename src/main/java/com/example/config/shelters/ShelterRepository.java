0788b5a5548912354eeb31dc4f7488b0da29925a
Рома <rsavcenko418@gmail.com>
2024-10-30 18:46:43 +0100

    version 1.3 Додано систему вибору тваринок

--- src/main/java/com/example/config/shelters/ShelterRepository.java
+++ src/main/java/com/example/config/shelters/ShelterRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Repository
 public interface ShelterRepository extends JpaRepository<Shelter, Long> {
     List<Shelter> findByCity(String city);
-    List<Shelter> findByAvailableAnimalsNotEmpty(); // Example method to find shelters with available animals
+    List<Shelter> findByAnimalsNotEmpty(); // Example method to find shelters with available animals
 
     boolean existsById(Long id);
 }
